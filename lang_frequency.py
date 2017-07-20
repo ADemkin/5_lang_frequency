56068fd0b5672b4708daaef9aa266297795a3b2f
antondemkin <antondemkin@yandex.ru>
2017-07-20 17:54:32 +0300

    Minor optimisations.

--- lang_frequency.py
+++ lang_frequency.py
@@ -11,18 +11,9 @@ def load_data(filepath):
     return text
 
 
-def remove_special_symbols(string):
-    # remove all special symbols and convert to lowercase
-    text = re.sub("[^A-Za-z0-9']+", " ", string)
-    text = text.lower()
-    return text
-
-
 def get_most_frequent_words(string):
     number_of_results = 10
-    # separate words by spaces
     all_words = string.split()
-    # count words and return top 10 only
     return collections.Counter(all_words).most_common(number_of_results)
 
 
@@ -32,8 +23,10 @@ def main():
         path = sys.argv[1]
         if os.path.exists(path):
             text = load_data(path)
-            filtered_text = remove_special_symbols(text)
-            top_ten_words_count = get_most_frequent_words(filtered_text)
+            # keep only letters, digits and '
+            filtered_text = re.sub("[^A-Za-z0-9']+", " ", text)
+            lowercase_text = filtered_text.lower()
+            top_ten_words_count = get_most_frequent_words(lowercase_text)
             for word in top_ten_words_count:
                 print(word[0])
 
