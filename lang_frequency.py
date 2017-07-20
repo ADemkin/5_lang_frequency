91f14b33ccc6014a37fa154cd3dd702a9bc3c5c4
antondemkin <antondemkin@yandex.ru>
2017-07-20 18:51:50 +0300

    Code readability improved.

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,14 +1,20 @@
 import re
-import operator
 import sys
 import os
 import collections
 
 
 def load_data(filepath):
-    with open(filepath) as file:
-        text = file.read()
-    return text
+    if os.path.exists(filepath):
+        with open(filepath) as file:
+            text = file.read()
+        return text
+
+
+def filter_words_and_convert_to_lowercase(text):
+    filtered_text = re.sub("[^A-Za-z0-9']+", " ", text)
+    lowercase_text = filtered_text.lower()
+    return lowercase_text
 
 
 def get_most_frequent_words(string):
@@ -17,18 +23,14 @@ def get_most_frequent_words(string):
     return collections.Counter(all_words).most_common(number_of_results)
 
 
-
 def main():
     if len(sys.argv) > 1:
         path = sys.argv[1]
-        if os.path.exists(path):
-            text = load_data(path)
-            # keep only letters, digits and '
-            filtered_text = re.sub("[^A-Za-z0-9']+", " ", text)
-            lowercase_text = filtered_text.lower()
-            top_ten_words_count = get_most_frequent_words(lowercase_text)
-            for word in top_ten_words_count:
-                print(word[0])
+        words_data = load_data(path)
+        lowercase_text = filter_words_and_convert_to_lowercase(words_data)
+        top_ten_words_count = get_most_frequent_words(lowercase_text)
+        for word in top_ten_words_count:
+            print(word[0])
 
 
 if __name__ == '__main__':
